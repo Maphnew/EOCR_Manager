9e9fb5967646e32b9a2ac3d616140b0eb5888630
maphnew <zcm3118@gmail.com>
2020-01-16 16:51:17 +0900

    await

--- src/utils/yaml.js
+++ src/utils/yaml.js
@@ -5,13 +5,13 @@ const writeYaml = async (publicDirectoryPath, inputHost) => {
     const yamlPath = '/etc/netplan'
     try {
         fs.readFile(publicDirectoryPath+'/01-network-manager-all.json', (err, data) => {
-            let jsonobj = await JSON.parse(data)
+            let jsonobj = JSON.parse(data)
             console.log('from default: ', jsonobj.network.ethernets.eth0.addresses[0])
-            jsonobj.network.ethernets.eth0.addresses[0] = await inputHost+'/24'
+            jsonobj.network.ethernets.eth0.addresses[0] = inputHost+'/24'
             console.log('to: ', jsonobj.network.ethernets.eth0.addresses[0])
-            let ymlText = await json2yaml.stringify(jsonobj)
+            let ymlText = json2yaml.stringify(jsonobj)
             // fs.writeFileSync(publicDirectoryPath+'/yaml-edit.yaml', ymlText, 'utf-8')
-            await fs.writeFileSync(yamlPath+'/01-network-manager-all.yaml', ymlText, 'utf-8')
+            fs.writeFileSync(yamlPath+'/01-network-manager-all.yaml', ymlText, 'utf-8')
         })
     } catch(e) {
         console.log(e)
